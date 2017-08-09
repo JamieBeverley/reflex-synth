bebce8757045b4a8268b0281f7a26071217496b6
Jamie Beverley <jamie_beverley@hotmail.com>
2017-08-09 11:17:03 -0400

    building but not working

--- src/Reflex/Synth/Synth.hs
+++ src/Reflex/Synth/Synth.hs
@@ -1,6 +1,8 @@
 module Reflex.Synth.Synth where
 
-import Reflex.Synth.Types
+import Reflex.Synth.Types 
+import Reflex.Dom
+import Control.Monad.IO.Class (liftIO)
 
 class WebAudio a where
   createNode :: a -> IO WebAudioNode
@@ -9,72 +11,90 @@ data Filter = NoFilter |
               PeakingFilter Double Double Double -- Frequency Q Gain
 
 instance WebAudio Filter where
-  createNode (NoFilter) = createGainNode 1.0
+  createNode (NoFilter) = createGain 1.0
   createNode (PeakingFilter f q g) = createPeakingFilter f q g
 
 data Source = PinkNoise
 
 instance WebAudio Source where
-  createNode (PinkNoise dur) = do
-    x <- createPinkNoiseNode
-    y <- createAsrEnvelope 0.005 dur 0.005
-    connect x y
+  createNode PinkNoise = createPinkNoise
+--instance WebAudio Source where
+  --createNode PinkNoise = do
+    --x <- createPinkNoise
+    --y <- createAsrEnvelope 0.005 1 0.005 --@  no envelope for a 'source' envelope only for duration -specific things (synths)
+    --connect x y
+
+
 
 data Synth = NoSynth | Synth Source Filter Double  -- the 'Double' is a synth duration
 
-instance WebAudio Synth where
-  createNode (NoSynth) = return NullAudioNode
-  createNode (Synth s f dur) = do
-    x <- createNode s
-    y <- createNode f
-    env <- createAsrEnvelope 0.05 dur 0.05
-    connect x y
-    connect y env
-    dest <- getDestination
-    connect env dest
+--instance WebAudio Synth where
+--  createNode (NoSynth) = return NullAudioNode
+--  createNode (Synth s f dur) = do
+--    x <- createNode s
+--    y <- createNode f
+--    env <- createAsrEnvelope 0.05 dur 0.05
+--    connect x y
+--    connect y env
+--    dest <- getDestination
+--    connect env dest
+
+
+--data WebAudioNode = WebAudioNode NodeType JSVal | NullAudioNode
 
 
 
-performSynth:: MonadWidget t m => Event t Synth -> m ()
-performSynth e = do
-  performEvent_ $ fmap play e
-  where 
-    play (Synth s f d) = 
-    play (NoSynth) = 
+--performSynth:: MonadWidget t m => Event t Synth -> m ()
+--performSynth event = do
+--  let n = fmap (\e-> do 
+--                      node <- createNode e
+--                      startNode node
+--                      ) event          -- Event t (IO ())
+--  performEvent_ $ fmap liftIO n
 
 
-performEvent_ :: MonadWidget t m => Event t (WidgetHost m ()) -> m ()
 
-iftIO :: MonadIO m => IO a -> m a
+  --createNode :: a -> IO WebAudioNode
 
-doHint :: WebDirt -> Hint -> IO ()
 
-performHint :: MonadWidget t m => WebDirt -> Event t Hint -> m ()
-performHint wd ev = performEvent_ $ fmap (liftIO . (doHint wd)) ev
+--startNode :: WebAudioNode -> IO ()
+
+--createNode :: a -> IO WebAudioNode
+
+--play::IO WebAudioNode -> IO WebAudioNode
+
+--performEvent_ :: MonadWidget t m => Event t (WidgetHost m ()) -> m ()
+
+--iftIO :: MonadIO m => IO a -> m a
+
+--doHint :: WebDirt -> Hint -> IO ()
+
+--performHint :: MonadWidget t m => WebDirt -> Event t Hint -> m ()
+--performHint wd ev = performEvent_ $ fmap (liftIO . (doHint wd)) ev
 
 
 
 -- an example of how this might be used with reflex-dom:
 
-example :: m (Event t ())
-example = do
-  play <- button "play"
-  let synthAction = createNode (Synth PinkNoise (PeakingFilter 1500 1.4 6.0)) <$ play
-  performEvent_ $ fmap liftIO synthAction
+--example :: m (Event t ())
+--example = do
+--  play <- button "play"
+--  let synthAction = createNode (Synth PinkNoise (PeakingFilter 1500 1.4 6.0)) <$ play
+--  performEvent_ $ fmap liftIO synthAction
 
-getNewQuestion :: StdGen -> IO (Synth,StdGen)
-getNewQuestion gen = do
-  (x,g) <- randomR (0,1) gen
-  let s = if x==0 then (Synth PinkNoise NoFilter) else (Synth PinkNoise (PeakingFilter 1500 1.4 6.0))
-  return (s,g)
+--getNewQuestion :: StdGen -> IO (Synth,StdGen)
+--getNewQuestion gen = do
+--  (x,g) <- randomR (0,1) gen
+--  let s = if x==0 then (Synth PinkNoise NoFilter) else (Synth PinkNoise (PeakingFilter 1500 1.4 6.0))
+--  return (s,g)
 
-example2 :: StdGen -> m ()
-example2 gen = el "div" $ mdo
-  newButton <- button "new"
-  gen'' <- tagPromptlyDyn gen' newButton -- m (Event t StdGen)
-  newQuestion <- performEvent $ fmap (liftIO . getNewQuestion) gen'' -- m (Event t (Synth,StdGen))
-  synth <- holdDyn (NoSynth) $ fmap fst newQuestion -- m (Dynamic t Synth)
-  gen' <- holdDyn gen $ fmap snd newQuestion -- m (Dynamic t StdGen)
-  playButton <- button "play"
-  synthAction <- tagPromptlyDyn synth playButton -- m (Event t Synth)
-  performEvent_ $ fmap (liftIO . createNode) synthAction
+--example2 :: StdGen -> m ()
+--example2 gen = el "div" $ mdo
+--  newButton <- button "new"
+--  gen'' <- tagPromptlyDyn gen' newButton -- m (Event t StdGen)
+--  newQuestion <- performEvent $ fmap (liftIO . getNewQuestion) gen'' -- m (Event t (Synth,StdGen))
+--  synth <- holdDyn (NoSynth) $ fmap fst newQuestion -- m (Dynamic t Synth)
+--  gen' <- holdDyn gen $ fmap snd newQuestion -- m (Dynamic t StdGen)
+--  playButton <- button "play"
+--  synthAction <- tagPromptlyDyn synth playButton -- m (Event t Synth)
+--  performEvent_ $ fmap (liftIO . createNode) synthAction
--- src/Reflex/Synth/Types.hs
+++ src/Reflex/Synth/Types.hs
@@ -11,8 +11,14 @@ data NodeType = Filter FilterType | Gain | Destination | Noise NoiseType
 
 data WebAudioNode = WebAudioNode NodeType JSVal | NullAudioNode
 
+-- JSVal 
+--data WebAudioGraph = WebAudioGraph WebAudioNode JSVal | WebAudioGraph' WebAudioNode WebAudioGraph
+
+
 getDestination :: IO WebAudioNode
-getDestination = F.getDestination >>= return $ WebAudioNode Destination
+getDestination = do
+  x <- F.getDestination 
+  return $ WebAudioNode Destination x
 
 connect :: WebAudioNode -> WebAudioNode -> IO ()
 connect (WebAudioNode Destination _) _ = error "destination can't be source of connection"
@@ -26,12 +32,12 @@ setGain _ _ = error "can't set gain value of node not of type Gain"
 
 createGain :: Double -> IO WebAudioNode
 createGain g = do
-  x <- F.createGain >>= WebAudioNode Gain
-  setGain g x
-  return x
+  x <- F.createGain
+  setGain g (WebAudioNode Gain x)
+  return (WebAudioNode Gain x)
 
 createBiquadFilter :: IO WebAudioNode
-createBiquadFilter = F.createBiQuadFilter >>= return $ WebAudioNode Filter
+createBiquadFilter = F.createBiquadFilter >>= return . WebAudioNode (Filter Peaking) 
 
 createPeakingFilter :: Double -> Double -> Double -> IO WebAudioNode
 createPeakingFilter f q g = do
@@ -44,38 +50,47 @@ createPeakingFilter f q g = do
 
 setFilterF :: Double -> WebAudioNode -> IO ()
 setFilterF f (WebAudioNode (Filter _) x) = do
-  F.setF f x
+  F.setF x f
   return ()
 setFilterF _ _ = error "can't setFilterF on non-filter"
 
 setFilterQ :: Double -> WebAudioNode -> IO ()
 setFilterQ q (WebAudioNode (Filter _) x) = do
-  F.setQ q x
+  F.setQ x q
   return ()
 setFilterQ _ _ = error "can't setFilterQ on non-filter"
 
 setFilterGain :: Double -> WebAudioNode -> IO ()
 setFilterGain g (WebAudioNode (Filter _) x) = do
-  F.setFilterGain g x
+  F.setFilterGain x g
   return ()
 setFilterGain _ _ = error "can't setFilterGain on non-filter"
 
 createWhiteNoise :: IO WebAudioNode
-createWhiteNoise = F.createWhiteNoise >>= return $ WebAudioNode (Noise White)
+createWhiteNoise = F.createWhiteNoise >>= return . WebAudioNode (Noise White)
 
 createPinkNoise :: IO WebAudioNode
-createPinkNoise = F.createPinkNoise >>= return $ WebAudioNode (Noise Pink)
+createPinkNoise = F.createPinkNoise >>= return . WebAudioNode (Noise Pink)
 
 createBrownianNoise :: IO WebAudioNode
-createBrownianNoise = F.createBrownianNoise >>= return $ WebAudioNode (Noise Brownian)
+createBrownianNoise = F.createBrownianNoise >>= return . WebAudioNode (Noise Brownian)
 
 createAsrEnvelope :: Double -> Double -> Double -> IO WebAudioNode
 createAsrEnvelope a s r = do
-  now <- getCurrentTime
-  n <- createGainNode'
+  now <- F.getCurrentTime
+  n <- createGain 1
   setGain 0.0 n
   setGainAtTime 0.0 now n
   setGainAtTime 1.0 (now+a) n
   setGainAtTime 1.0 (now+a+s) n
   setGainAtTime 0.0 (now+a+s+r) n
   return n
+
+
+setGainAtTime:: Double -> Double -> WebAudioNode -> IO ()
+setGainAtTime val t (WebAudioNode _ node) = F.setGainAtTime val t node
+setGainAtTime _ _ NullAudioNode = error "Cannot set gain of a null node"
+
+startNode :: WebAudioNode -> IO ()
+startNode (WebAudioNode _ ref) = F.startNode ref
+startNode _ = return ()
